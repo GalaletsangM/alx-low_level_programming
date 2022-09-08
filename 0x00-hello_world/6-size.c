ff1ef67f0cf12367621887075c1fe277a974070b
Your Name <you@example.com>
2022-09-08 15:51:53 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return : Always 0(Success)
+*/
+int main(void)
+{
+	int intType;
+	float floatType;
+	char charType;
+	long int longIntType;
+	long long int longLongIntType;
+
+	printf("Size of  char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+	printf("Size of a float: %zu byte(S)\n", sizeof(floatType));
+	return (0);
+}
