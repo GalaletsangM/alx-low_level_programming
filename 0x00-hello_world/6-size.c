8d30c57b7bdd44ebeb1ef9bec3d7b11f7719af89
Your Name <you@example.com>
2022-09-08 16:02:27 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long int longIntType;
 	long long int longLongIntType;
 
-	printf("Size of  char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of  char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
+	printf("Size of a float: %u byte(s)\n", sizeof(floatType));
 	return (0);
 }
