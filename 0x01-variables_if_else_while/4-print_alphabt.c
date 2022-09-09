39ce8a538a2200f93a625bf138402f4b2074c3f9
Your Name <you@example.com>
2022-09-09 15:01:49 -0700

    test

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: eclude q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == q || c == e)
+			continue;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
