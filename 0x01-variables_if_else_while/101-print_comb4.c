15c054f5795720d62d92fc74756fecb5d721450f
Your Name <you@example.com>
2022-09-10 12:45:29 -0700

    t

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print combination of three numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a;
+	long int b;
+	long int c;
+
+	for  (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			for (c = 1; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a == 7 || b == 8 || c == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
