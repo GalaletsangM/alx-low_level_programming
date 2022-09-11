51612d7131f902081514b2e9168a480b14c22d3f
Your Name <you@example.com>
2022-09-11 09:35:35 -0700

    t

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,28 +11,20 @@ int main(void)
 {
 	long int a;
 	long int b;
-	long int c;
-	long int d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0 ; b <= 9; b++)
+		for (b = 0 ; b <= 99; b++)
 		{
-			for (c = 0; c <= 9; c++)
-			{
-				for (d = c + 1; d <= 9; d++)
-				{
-					putchar((a / 10) + '0');
-					putchar((b % 10) + '0');
-					putchar(' ');
-					putchar((c / 10) + '0');
-					putchar((d % 10) + '0');
-					if (a == 9 && b == 8 && c == 9 && d == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((a / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar(' ');
+			putchar((a / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98  && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
