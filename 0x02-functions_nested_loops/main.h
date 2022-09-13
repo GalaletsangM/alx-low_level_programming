3e465f1fa1dcf0b0ce462d5ae8568d158695f927
Your Name <you@example.com>
2022-09-13 15:42:53 -0700

    t

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef "main.h"
+#define "main.h"
+/**
+ * print_alphabet - printing alphabets
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+}
+#endif
