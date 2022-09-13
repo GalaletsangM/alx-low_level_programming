aa84e2dfe6b97ec0dcae1af7daca06d868bf84d8
Your Name <you@example.com>
2022-09-13 14:23:21 -0700

    t

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,6 +12,9 @@ void print_alphabet(void)
 	}
 }
 
+/**
+ * print_alphabet - prints alphabets
+ */
 void print_alphabet(void);
 
 /**
@@ -23,6 +26,6 @@ void print_alphabet(void);
  */
 int main(void)
 {
-	print_alphabet(void);
+	print_alphabet();
 	return (0);
 }
