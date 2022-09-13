03c2d156f4b1a711c33c005d96ea9badfde25a69
Your Name <you@example.com>
2022-09-13 12:09:30 -0700

    t

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * print_alphabet - print alphabets
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(c + '\n');
+	return (0);
+}
