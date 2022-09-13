759f1e762550ed0dc33844484f4f32cae76730d2
Your Name <you@example.com>
2022-09-13 11:57:19 -0700

    t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 /**
+ * print_string - print string
+ *
+ * @s: string
+ */
+
+void print_string(char *s);
+
+/**
  * main - Entry point
  *
- * Description: print _putchar
+ * Description: Print string
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	_putchar('_putchar');
+	char s[] = "_putchar";
+
+	print_string(s);
+	putchar('\n');
 	return (0);
+}
+
+/**
+ * print_string - print string
+ *
+ * @s: string
+ */
+
+void print_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
 }
