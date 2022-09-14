b325c2adeffd4d5ad57df6ef361b56f3fe83985f
Your Name <you@example.com>
2022-09-14 12:42:23 -0700

    t

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - check if input is alphabet
+ * @c: letter
+ * Return: 1 if c is a letter
+ * On error, 0 is returned
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return(1);
+	else
+		return(0);
+}
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 'a';
+
+	_isalpha(c);
+	return (0);
+}
