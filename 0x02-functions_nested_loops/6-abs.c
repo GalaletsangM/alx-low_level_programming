e4393881924f48d820d1b017050de684624c889d
Your Name <you@example.com>
2022-09-14 23:47:55 -0700

    t

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - print the absolute value
+ * @int: integer
+ *Return: Absolute value
+ */
+int _abs(int)
+{
+	int j;
+
+	return (_abs(j));
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	_abs(int i);
+	return (0);
+}
