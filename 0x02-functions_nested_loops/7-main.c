64d48aeb8a8cdf5067f5ef4e28b4d93e7d4cd574
Your Name <you@example.com>
2022-09-15 06:02:30 -0700

    t

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(548);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - print the last digit
+ * @i: integer
+ * Return: Answer
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
