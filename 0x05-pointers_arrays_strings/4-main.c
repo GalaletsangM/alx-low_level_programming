5e978925c7973f583e3544639c26a98dd7b0670a
Your Name <you@example.com>
2022-09-19 14:52:42 -0700

    t

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char *s;
+
+	print_rev(s);
+	return (0);
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
