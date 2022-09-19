8f87157a21f9328da9d87fba25fcb72d22f7290b
Your Name <you@example.com>
2022-09-19 12:50:28 -0700

    t

---
+++ 0x05-pointer_array_strings/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int p;
+
+	p = 15;
+	reset_to_98(p);
+	return (0);
+}
---
+++ 0x05-pointer_array_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - reset to 98
+ * @*n: integer
+ */
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = 98;
+	n = &p;
+}
