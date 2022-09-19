bbfdbfa5a4684f50b7aa775be0be103294bb66e0
Your Name <you@example.com>
2022-09-19 08:02:11 -0700

    t

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 2;
+	int y = 3;
+
+	mul(x, y);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiply 2 integers
+ * @a: first input
+ * @b: second input
+ * Return: n value
+ */
+int mul(int a, int b)
+{
+	int n;
+
+	n =  a * b;
+	return (n);
+}
