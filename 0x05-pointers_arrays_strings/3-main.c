0a55d1c65f019bdfb7aa8b893e884d925f441205
Your Name <you@example.com>
2022-09-19 14:45:11 -0700

    t

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string
+ * @str: string input
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
