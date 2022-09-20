e4c244e9411d8d18b473adfea3593b6f778a4c40
Your Name <you@example.com>
2022-09-20 02:36:28 -0700

    t

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,14 @@ void print_rev(char *s)
 	int end;
 	int count = 0;
 
-	while (*s[count] != '\0')
+	while (s[count] != '\0')
 	{
 		count++;
 	}
 	end = count - 1;
 	for (begin = 0; begin < count; begin++)
 	{
-		r[begin] = *s[end];
+		r[begin] = s[end];
 		end--;
 	}
 	r[begin] = '\0';
