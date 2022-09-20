0274a85467b41b2504d20da802edf3afa7e4f7fa
Your Name <you@example.com>
2022-09-20 13:43:26 -0700

    t

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char n[10] = "My School";
+	rev_string(n);
+	printf("%s\n", n);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - print reverse string
+ * @s: string input
+ */
+void rev_string(char *s)
+{
+	int i;
+	int len;
+	int temp;
+	
+	len = strlen(s);
+	for (i = 0; i < len / ; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
