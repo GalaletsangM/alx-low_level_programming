4d63cbbaa5d9e6ab1d137ecdc6a6672aff636c6b
Your Name <you@example.com>
2022-09-20 13:50:09 -0700

    t

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * rev_string - print reverse string
  * @s: string input
@@ -11,7 +12,7 @@ void rev_string(char *s)
 	int temp;
 	
 	len = strlen(s);
-	for (i = 0; i < len / ; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
