d1b7776e913deaac50c3c4bbc93e3b2e60406405
Your Name <you@example.com>
2022-09-20 17:54:41 -0700

    t

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _atoi - string to integer
+ * @s: string
+ */
+int _atoi(char *s)
+{
+	int i;
+
+	i = atoi(s);
+	return (i);
+}
---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	_atoi("45");
+	return (0);
+}
