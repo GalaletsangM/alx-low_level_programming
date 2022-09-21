fd55beec3a6ee40ee54dc496345f93cfa940dc2c
Your Name <you@example.com>
2022-09-20 17:32:18 -0700

    t

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	x = 20;
+	*_strcpy(y, x);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - copy
+ * @dest: buffer
+ * @src: string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
