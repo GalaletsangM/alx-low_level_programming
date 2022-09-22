050608a2621268a5486f60f9c72eb49ad632f759
Your Name <you@example.com>
2022-09-22 02:25:22 -0700

    t

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char a[] = "Hello ";
+	char b[] = "World\n";
+	char *ptr;
+
+	ptr = _strcat(a, b);
+	printf("%s", ptr);
+	putchar('\n');
+	return (0);
+}
