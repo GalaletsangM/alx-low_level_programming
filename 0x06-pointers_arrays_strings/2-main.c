889b1d70d6718eb15812e79be8edc915caaa2cdd
Your Name <you@example.com>
2022-09-22 03:16:06 -0700

    t

---
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char a[98] = "My name is Galaletsang";
+	char b[50];
+
+	_strncpy(b, a, 7);
+	printf("%s", b);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * *_strncpy - copies a string
+ * @dest: destination
+ * @src: source code
+ * @n: integer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
