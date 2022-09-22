4c04ab85c6626e2aafe3f35a69a7034ad96dd86d
Your Name <you@example.com>
2022-09-22 03:30:38 -0700

    t

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	char a[98] = "My name is Galaletsang";
 	char b[50];
+	int n = 7;
 
-	_strncpy(b, a, 7);
+	_strncpy(b, a, n);
+	b[n] = '\0';
 	printf("%s", b);
-	putchar('\n');
 	putchar('\n');
 	return (0);
 }
