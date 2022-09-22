72ce67c3d1a9efff219651e4772c22d8776c71e6
Your Name <you@example.com>
2022-09-22 09:23:32 -0700

    t

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j;
-	int k = 0;
+	int k;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			break;
-		}
-		i++;
-	}
-	for (i = 0; s1[i] != '\0'; ++i)
-
-	for (j = 0; s2[j] != '\0'; ++j)
-
-	if (i != j)
-	{
-		k = s1[i] - s2[j];
-	}
-	else
-	{
-		k = 0;
-	}
+	k = strcmp(s1, s2);
 	return (k);
 }
