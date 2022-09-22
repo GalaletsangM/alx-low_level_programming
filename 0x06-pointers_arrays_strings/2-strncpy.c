ae103d0de8b4e35f87a4f0ce4e48327102806c1a
Your Name <you@example.com>
2022-09-22 06:36:46 -0700

    t

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,9 +6,11 @@
  * @dest: destination
  * @src: source code
  * @n: integer
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
+	dest[i] = '\0';
 	return (dest);
 }
