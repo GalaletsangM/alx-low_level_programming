82980972e8f57cd7280dcabfd7d81911bc77da91
Your Name <you@example.com>
2022-09-27 11:39:33 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,18 +9,13 @@ void print_chessboard(char (*a)[8])
 {
 	int i;
 	int j;
-	char chess[8][8];
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			(*a)[8] = chess[i][j];
-			printf("%c ", (*a)[8]);
-			if (j == 8)
-			{
-				printf("\n");
-			}
+			putchar(a[i][j]);
 		}
+		putchar('\n');
 	}
 }
