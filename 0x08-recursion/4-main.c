78d66e4438e43f64c46220a72c72c7ba39c43ca6
Your Name <you@example.com>
2022-09-29 16:48:00 -0700

    power

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - check code
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	return (0);
+}
