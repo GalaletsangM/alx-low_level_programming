d709d57b997edc08a6c9af507761dcec028804f3
Your Name <you@example.com>
2022-09-29 16:33:53 -0700

    factorial

---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - check code
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	r = factorial(4);
+	printf("%d\n", r);
+	return (0);
+}
