e2119d9e504382e28a24f3a439a7b6134ee2b95e
Your Name <you@example.com>
2022-09-29 16:12:21 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		printf("%c", *s);
+	}
+}
