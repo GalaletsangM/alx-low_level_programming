9ac80e534c3fd6b7e632845da5f4935f83511cb4
Your Name <you@example.com>
2022-10-04 11:31:24 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
