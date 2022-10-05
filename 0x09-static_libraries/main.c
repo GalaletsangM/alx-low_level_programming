ffc7e0eb9fccb9b2fe5ea303295a9d383e56cb68
Your Name <you@example.com>
2022-10-05 06:06:12 -0700

    test

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check code
+ * *Return: 0
+ */
+int main(void)
+{
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
