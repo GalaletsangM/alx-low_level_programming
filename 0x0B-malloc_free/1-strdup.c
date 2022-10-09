2bf7290ac35071c109c3e2c54b4da940f02f8792
Your Name <you@example.com>
2022-10-09 14:03:17 -0700

    test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,13 @@ char *_strdup(char *str)
 {
 	char *c;
 
-	str = malloc(sizeof(char) * strlen(str));
+	c = malloc(sizeof(char) * strlen(str));
 
-	if (str == NULL)
+	if (str == NULL || c == NULL)
 	{
 		printf("failed to allocate memory");
 	}
 	c = strdup(str);
-	if (c == NULL)
-	{
-		printf("failed to allocate memory");
-	}
 	return (c);
+	free(c);
 }
