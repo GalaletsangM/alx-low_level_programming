ef46a3c1a08eadbe02fd7645c7d426286fc08c7b
Your Name <you@example.com>
2022-10-10 10:25:39 -0700

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - name, age and owner of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner of the dog
+ *
+ * Description: a new type struct with the 3 given elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
