a1db05361c29cb99585ea52821edacee827ef3cd
Your Name <you@example.com>
2022-10-10 09:04:11 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,15 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *a;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
-	else if (s1 == NULL && s2 != NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	else if (s1 != NULL && s2 == NULL)
+	if (s1 != NULL)
 	{
 		s2 = "";
 	}
