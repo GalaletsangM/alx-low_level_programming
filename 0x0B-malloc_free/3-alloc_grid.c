b6a1e3e718f9a31fc9da43b90ee134366ae54bd7
Your Name <you@example.com>
2022-11-13 02:41:14 -0800

    pointer to 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of an array
+ * @height: height of an array
+ * Return: returns array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i;
+
+	arr = (int **)malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+	}
+	return (arr);
+}
