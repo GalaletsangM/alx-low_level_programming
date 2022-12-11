188597ec8ecbfb1a51216265f34440327625a017
Your Name <you@example.com>
2022-12-11 15:58:06 -0800

    execute a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - execute a function
+ * @array: array to be executed
+ * @action: pointer to a funtion
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	while (size > 0)
+	{
+		action(*array)
+	}
+}
