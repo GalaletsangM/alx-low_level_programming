8a3f0e691ab720e35a4b34d4ab499439c234a472
Your Name <you@example.com>
2023-06-10 16:34:29 -0700

    static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -6,4 +9,5 @@
  */
 int _atoi(char *s)
 {
+	return (atoi(*s));	
 }
