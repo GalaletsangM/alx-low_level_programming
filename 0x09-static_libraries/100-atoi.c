974075430c0d41505fc7e4eaffcfc4d1438488b0
Your Name <you@example.com>
2023-06-10 19:35:35 -0700

    static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,13 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: integer
  */
 int _atoi(char *s)
 {
-	return (atoi(*s));	
+	int sig = 1;
+	int i = 0;
+	unsigned int unsig = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		unsig = (unsig * 10) + (s[i] - '0');
+		i++;
+	}
+	unsig *= sig;
+	return (unsig);
 }
