eb9f3f8eda5baca4eb132f73964a7cdb6c8963ea
Your Name <you@example.com>
2023-06-21 15:38:34 -0700

    more malloc and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * *_calloc - allocate memory of an array
+ * _calloc - allocate memory of an array
  * @nmemb: number of elements to be allocated
  * @size: size of elements
  *
- * Return: nothing
+ * Return: array
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *c;
+	char *c;
 	int i = 0, l = 0;
 
 	if (nmemb == 0 || size == 0)
