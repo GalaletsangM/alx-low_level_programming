fdb5326937c31d7c77bec42b8e03131b5ddcc150
Your Name <you@example.com>
2023-08-21 18:31:17 -0700

    variadic function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: format of variable
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *seperator = ";";
+	char *string;
+	va_list var;
+
+	va_start(var, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch(format[i])
+			{
+				case 'i':
+					printf("%d ", va_arg(var, int));
+					break;
+				case 'c':
+					printf("%c ", va_arg(var, int));
+					break;
+				case 'f':
+					printf("%f ", va_arg(var, double));
+					break;
+				case 's':
+					string = va_arg(var, char*);
+
+					if (string == NULL)
+						printf("(nil)");
+					printf("%s ", string);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			printf("%s ", seperator);
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(var);
+}
